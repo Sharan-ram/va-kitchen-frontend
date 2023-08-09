f92acadea300f9d14139f2f0f894d4eb3b62bb66
sharan-aura <sharan@auranft.co>
2023-08-09 22:00:02 +0530

    Render recipes dropdown for meal plan

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,11 +33,14 @@ const Homepage = () => {
   const [headcount, setHeadcount] = useState();
   const [veganCount, setVeganCount] = useState();
   const [nonVeganCount, setNonVeganCount] = useState();
+  const [mealPlan, setMealPlan] = useState({});
 
   console.log({ recipes, ingredients });
 
   // Function to check if a date is Sunday
   const isSunday = (date) => date.getDay() === 0;
+
+  console.log({ mealPlan });
 
   return (
     <div>
@@ -78,14 +81,35 @@ const Homepage = () => {
             </tr>
           </thead>
           <tbody>
-            {daysOfMonth.map((day, index) => (
-              <tr className="border border-black" key={day}>
-                <td>{`${day.getDate()}, ${weekDays[day.getDay()]}`}</td>
-                {meals.map((meal) => (
-                  <td key={`${day}-${meal}`}>Content for {meal}</td>
-                ))}
-              </tr>
-            ))}
+            {daysOfMonth.map((day, index) => {
+              const date = day.getDate();
+              const dayName = day.getDay();
+              return (
+                <tr className="border border-black" key={day}>
+                  <td>{`${date}, ${weekDays[dayName]}`}</td>
+                  {meals.map((meal) => (
+                    <td key={`${date}-${meal}`}>
+                      <select
+                        selected={
+                          mealPlan[`${date}-${meal}`] || "Select Recipe"
+                        }
+                        onChange={(e) => {
+                          setMealPlan({
+                            ...mealPlan,
+                            [`${date}-${meal}`]: e.target.value,
+                          });
+                        }}
+                      >
+                        <option value="Select Recipe">Select Recipe</option>
+                        {Object.keys(recipes).map((option) => {
+                          return <option value={option}>{option}</option>;
+                        })}
+                      </select>
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
             {daysOfMonth.some(isSunday) && <tr style={{ height: "30px" }} />}
           </tbody>
         </table>
