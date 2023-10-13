2c6f07a1059a639255213873777251757f37b7a5
sharan-aura <sharan@auranft.co>
2023-10-13 15:33:15 +0530

    Fix changes for dropdown render in recipes

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,12 +1,9 @@
-export function generateDaysOfMonth() {
-  const currentDate = new Date();
-  const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth();
-  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // Get the last day of the current month
+export function generateDaysOfMonth(year, month) {
+  const daysInMonth = new Date(year, month + 1, 0).getDate(); // Get the last day of the specified month
 
   const days = [];
   for (let day = 1; day <= daysInMonth; day++) {
-    days.push(new Date(currentYear, currentMonth, day));
+    days.push(new Date(year, month, day));
   }
 
   return days;
