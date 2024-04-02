163118c9da70bfc907b69a27d39c8fdd29c21135
Sharan R <sharanram1993@gmail.com>
2024-04-02 17:39:12 +0530

    Fixup

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -20,7 +20,7 @@ const Input = ({ type = "text", textInputProps, selectProps, classes }) => {
         // className="rounded-md border border-black pl-2 w-full cursor-pointer"
         className={classNames(
           "rounded-md border border-gray-300 p-2 w-full",
-          classes.wrapper
+          classes?.wrapper
         )}
       >
         {options.map((option, index) => {
