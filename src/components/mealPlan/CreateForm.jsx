ff7b2e8d1713b1bf02c346497b516ac9f77cf51f
Sharan R <sharanram1993@gmail.com>
2024-08-23 13:16:11 +0530

    Style selection form for create meal plan

--- src/components/mealPlan/CreateForm.jsx
+++ src/components/mealPlan/CreateForm.jsx
@@ -60,110 +60,111 @@ const MealPlanForm = ({
 
   return (
     <div>
-      <div className="flex">
-        <Input
-          type="select"
-          selectProps={{
-            selected: year,
-            onChange: (e) => setYear(e.target.value),
-            options: [{ value: "", text: "Select year" }, ...years],
-          }}
-          classes={{ wrapper: "w-1/4 mr-4 mb-4" }}
-        />
-        <Input
-          type="select"
-          selectProps={{
-            selected: month,
-            onChange: (e) => setMonth(e.target.value),
-            options: [{ value: "", text: "Select month" }, ...months],
-          }}
-          classes={{ wrapper: "w-1/4 mr-4 mb-4" }}
-        />
-        <button
-          onClick={fetchMealPlan}
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-        >
-          Next
-        </button>
+      <div className="flex justify-between w-full">
+        <div className="w-[85%] flex items-center justify-between">
+          <div className="w-[45%]">
+            <Input
+              type="select"
+              selectProps={{
+                selected: year,
+                onChange: (e) => setYear(e.target.value),
+                options: [{ value: "", text: "Select year" }, ...years],
+              }}
+            />
+          </div>
+
+          <div className="w-[45%]">
+            <Input
+              type="select"
+              selectProps={{
+                selected: month,
+                onChange: (e) => setMonth(e.target.value),
+                options: [{ value: "", text: "Select month" }, ...months],
+              }}
+            />
+          </div>
+        </div>
+        <div className="w-[15%] text-right">
+          <button
+            onClick={fetchMealPlan}
+            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          >
+            Next
+          </button>
+        </div>
       </div>
 
       {!loadingMealPlan && step === 2 && (
-        <div className="">
-          <Input
-            type="select"
-            key={season}
-            selectProps={{
-              selected: season,
-              onChange: (e) => setSeason(e.target.value),
-              options: [{ value: "", text: "Select season" }, ...seasons],
-              // value: season,
-            }}
-            classes={{ wrapper: "w-1/4 mr-4 mb-4" }}
-          />
-          <div className="w-1/4 mr-4 mb-4">
-            <label
-              htmlFor="veganCount"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Vegan Count
-            </label>
-            <input
-              type="number"
-              id="veganCount"
-              value={monthCounts.veganCount}
-              onChange={(e) =>
-                setMonthCounts({
-                  ...monthCounts,
-                  veganCount: Number(e.target.value),
-                })
-              }
-              placeholder="Vegan Count"
-              className="w-full rounded-md border border-gray-300 p-2"
-            />
+        <div className="w-full flex justify-between items-end mt-6">
+          <div className="w-[85%] flex items-end justify-between">
+            <div className="w-[20%]">
+              <Input
+                type="select"
+                key={season}
+                selectProps={{
+                  selected: season,
+                  onChange: (e) => setSeason(e.target.value),
+                  options: [{ value: "", text: "Select season" }, ...seasons],
+                  // value: season,
+                }}
+              />
+            </div>
+            <div className="w-[20%]">
+              <label htmlFor="veganCount" className="block font-semibold">
+                Vegan Count
+              </label>
+              <input
+                type="number"
+                id="veganCount"
+                value={monthCounts.veganCount}
+                onChange={(e) =>
+                  setMonthCounts({
+                    ...monthCounts,
+                    veganCount: Number(e.target.value),
+                  })
+                }
+                placeholder="Vegan Count"
+                className="w-full rounded-md border border-gray-300 p-2"
+              />
+            </div>
+            <div className="w-[20%]">
+              <label htmlFor="nonVeganCount" className="block font-semibold">
+                Non-Vegan Count
+              </label>
+              <input
+                type="number"
+                id="nonVeganCount"
+                value={monthCounts.nonVeganCount}
+                onChange={(e) =>
+                  setMonthCounts({
+                    ...monthCounts,
+                    nonVeganCount: Number(e.target.value),
+                  })
+                }
+                placeholder="Non-Vegan Count"
+                className="w-full rounded-md border border-gray-300 p-2"
+              />
+            </div>
+            <div className="w-[20%]">
+              <label htmlFor="glutenFreeCount" className="block font-semibold">
+                Gluten-Free Count
+              </label>
+              <input
+                type="number"
+                id="glutenFreeCount"
+                value={monthCounts.glutenFreeCount}
+                onChange={(e) =>
+                  setMonthCounts({
+                    ...monthCounts,
+                    glutenFreeCount: Number(e.target.value),
+                  })
+                }
+                placeholder="Gluten-Free Count"
+                className="w-full rounded-md border border-gray-300 p-2"
+              />
+            </div>
           </div>
-          <div className="w-1/4 mr-4 mb-4">
-            <label
-              htmlFor="nonVeganCount"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Non-Vegan Count
-            </label>
-            <input
-              type="number"
-              id="nonVeganCount"
-              value={monthCounts.nonVeganCount}
-              onChange={(e) =>
-                setMonthCounts({
-                  ...monthCounts,
-                  nonVeganCount: Number(e.target.value),
-                })
-              }
-              placeholder="Non-Vegan Count"
-              className="w-full rounded-md border border-gray-300 p-2"
-            />
-          </div>
-          <div className="w-1/4 mr-4 mb-4">
-            <label
-              htmlFor="glutenFreeCount"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Gluten-Free Count
-            </label>
-            <input
-              type="number"
-              id="glutenFreeCount"
-              value={monthCounts.glutenFreeCount}
-              onChange={(e) =>
-                setMonthCounts({
-                  ...monthCounts,
-                  glutenFreeCount: Number(e.target.value),
-                })
-              }
-              placeholder="Gluten-Free Count"
-              className="w-full rounded-md border border-gray-300 p-2"
-            />
-          </div>
-          <div className="w-full mb-4">
+          <div className="w-[15%] text-right">
             <button
               onClick={handleShowMealPlan}
               className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
