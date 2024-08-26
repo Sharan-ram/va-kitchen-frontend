68b5c8a7cecaf8a1d1025eca8478ea84d892e46e
Sharan R <sharanram1993@gmail.com>
2024-08-26 16:11:53 +0530

    Add loaders to prices update page

--- src/pages/ingredients/price/manual-update.jsx
+++ src/pages/ingredients/price/manual-update.jsx
@@ -1,16 +1,26 @@
 import { useState, useEffect } from "react";
 import { getIngredientPrices, updatePrices } from "@/services/ingredient";
 
+import { toast } from "react-toastify";
+import Loader from "@/components/Loader";
+import classNames from "classnames";
+
 const ManualPriceUpdate = () => {
   const [ingredients, setIngredients] = useState();
+  const [pricesLoading, setPricesLoading] = useState(false);
+  const [updatePricesLoading, setUpdatePricesLoading] = useState(false);
 
   useEffect(() => {
     const fetchIngredients = async () => {
       try {
+        setPricesLoading(true);
         const res = await getIngredientPrices();
         setIngredients(res);
+        setPricesLoading(false);
       } catch (e) {
         console.error(e);
+        setPricesLoading(false);
+        toast.error("Error fetching prices!");
       }
     };
 
@@ -20,6 +30,7 @@ const ManualPriceUpdate = () => {
   //   console.log({ ingredients });
   const updatePrice = async () => {
     try {
+      setUpdatePricesLoading(true);
       const newIngredients = ingredients.map((ing) => {
         return {
           ...ing,
@@ -29,15 +40,22 @@ const ManualPriceUpdate = () => {
       await updatePrices({
         ingredients: newIngredients,
       });
+      setUpdatePricesLoading(false);
+      toast.success("Prices updated successfully!");
     } catch (e) {
       console.error(e);
+      setUpdatePricesLoading(false);
+      toast.error("Error updating prices!");
     }
   };
+
+  const disableUpdateButton =
+    pricesLoading || updatePricesLoading || !ingredients;
 
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4">Manual Price Update</h2>
-      {ingredients &&
+      {ingredients && !pricesLoading ? (
         ingredients.map((ingredient) => {
           return (
             <div key={ingredient._id} className="flex items-center w-1/2 my-4">
@@ -72,12 +90,21 @@ const ManualPriceUpdate = () => {
               </div>
             </div>
           );
-        })}
+        })
+      ) : (
+        <div className="flex items-center justify-center">
+          <Loader />
+        </div>
+      )}
       <div className="mt-10">
         <button
           type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+          className={classNames(
+            "px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600",
+            disableUpdateButton && "cursor-not-allowed opacity-50"
+          )}
           onClick={updatePrice}
+          disabled={disableUpdateButton}
         >
           Update Price
         </button>
