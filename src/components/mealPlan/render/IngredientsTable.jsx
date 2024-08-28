0044ef340ef1b96edb14f67c8a1f41032348a7a5
Sharan R <sharanram1993@gmail.com>
2024-08-28 13:50:38 +0530

    Fixup

--- src/components/mealPlan/render/IngredientsTable.jsx
+++ src/components/mealPlan/render/IngredientsTable.jsx
@@ -4,7 +4,6 @@ const IngredientsTable = ({ mealPlan }) => {
     mealPlan.forEach((mealPlanObj) => {
       mealPlanObj.days.forEach((day) => {
         const date = day.date;
-
         ["breakfast", "lunch", "dinner"].forEach((mealType) => {
           const meal = day[mealType];
           if (meal) {
@@ -12,19 +11,30 @@ const IngredientsTable = ({ mealPlan }) => {
             const mealName =
               mealType.charAt(0).toUpperCase() + mealType.slice(1);
 
+            const season = day.season || mealPlanObj.season;
+
             meal.recipes.forEach((recipe) => {
-              const count =
-                recipe.dietType === "vegan"
-                  ? mealCounts.veganCount
-                  : mealCounts.nonVeganCount;
+              const recipeName = recipe.name;
+              const splitRecipe = recipeName.split("-");
+              // console.log({ splitRecipe });
+
+              let count;
+              if (splitRecipe[1]) {
+                count =
+                  splitRecipe[1].trim().toLowerCase() === "Vegan".toLowerCase()
+                    ? mealCounts.veganCount
+                    : mealCounts.nonVeganCount;
+              } else {
+                count = mealCounts.veganCount + mealCounts.nonVeganCount;
+              }
 
               let recipePrice = 0;
 
               recipe.ingredients.forEach((ingredient, index) => {
                 const ingredientPrice =
-                  ingredient[mealPlanObj.season] *
+                  ingredient[season] *
                   count *
-                  ingredient.ingredient.price;
+                  (ingredient.ingredient.price || 0);
 
                 recipePrice = recipePrice + ingredientPrice;
 
@@ -33,7 +43,7 @@ const IngredientsTable = ({ mealPlan }) => {
                   Meal: index === 0 ? mealName : "",
                   RecipeName: index === 0 ? recipe.name : "",
                   IngredientName: ingredient.ingredient.name,
-                  Quantity: (ingredient[mealPlanObj.season] * count).toFixed(3),
+                  Quantity: (ingredient[season] * count).toFixed(3),
                   VeganCount: index === 0 ? mealCounts.veganCount : "",
                   NonVeganCount: index === 0 ? mealCounts.nonVeganCount : "",
                   ingredientPrice: ingredientPrice.toFixed(2),
@@ -64,34 +74,30 @@ const IngredientsTable = ({ mealPlan }) => {
     <div>
       <table className="min-w-full divide-y divide-gray-200 max-w-[100%]">
         <thead className="bg-gray-50 max-w-[100%]">
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Date
-          </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Meal
-          </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">Date</th>
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">Meal</th>
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Recipe
           </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Vegan Count
           </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Non Vegan Count
           </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Ingredient
           </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Quantity
           </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Ingredient Price
           </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Recipe Price
           </th>
-          <th className="px-3 py-2 text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th className="px-3 py-2 font-bold uppercase tracking-wider">
             Total Price
           </th>
         </thead>
@@ -99,13 +105,13 @@ const IngredientsTable = ({ mealPlan }) => {
         <tbody className="bg-white divide-y divide-gray-200 max-w-[100%]">
           {getRows().map((row, index) => (
             <tr key={index} className="border-b max-w-[100%]">
-              <td className="px-3 py-2 whitespace-nowrap capitalize">
+              <td className="px-3 py-2 whitespace-nowrap capitalize font-semibold">
                 {row.Date && row.Date}
               </td>
-              <td className="px-3 py-2 whitespace-nowrap capitalize">
+              <td className="px-3 py-2 whitespace-nowrap capitalize font-semibold">
                 {row.Meal && row.Meal}
               </td>
-              <td className="px-3 py-2 whitespace-nowrap capitalize">
+              <td className="px-3 py-2 whitespace-nowrap capitalize font-semibold">
                 {row.RecipeName && row.RecipeName}
               </td>
               <td className="px-3 py-2 whitespace-nowrap capitalize">
@@ -123,10 +129,10 @@ const IngredientsTable = ({ mealPlan }) => {
               <td className="px-3 py-2 whitespace-nowrap capitalize">
                 {row.ingredientPrice}
               </td>
-              <td className="px-3 py-2 whitespace-nowrap capitalize">
+              <td className="px-3 py-2 whitespace-nowrap capitalize font-semibold">
                 {row.recipePrice}
               </td>
-              <td className="px-3 py-2 whitespace-nowrap capitalize">
+              <td className="px-3 py-2 whitespace-nowrap capitalize font-bold">
                 {row.totalPrice}
               </td>
             </tr>
