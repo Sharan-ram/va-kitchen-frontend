7d0244dc5d129e2286527710e6c39b03ef133935
Sharan R <sharanram1993@gmail.com>
2024-09-06 19:18:03 +0530

    Show ingredients for a recipe in view all recipes page

--- src/pages/ingredients/view.jsx
+++ src/pages/ingredients/view.jsx
@@ -71,7 +71,7 @@ const Ingredients = () => {
       </div>
       <div className="mt-6">
         <table className="min-w-full divide-y divide-gray-200 max-w-[100%] mr-[40px]">
-          <thead className="bg-gray-50 max-w-[100%]">
+          <thead className="bg-gray-50 max-w-[100%] sticky top-[100px]">
             <tr className="bg-gray-200">
               <th className="px-3 py-2 font-bold uppercase tracking-wider">
                 No.
