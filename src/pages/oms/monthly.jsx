6d2e203d30f5c3e3b890c1a9680a5623202a35c4
Sharan R <sharanram1993@gmail.com>
2024-09-10 12:36:50 +0530

    Render monthly orders based on vendor and other filters

--- src/pages/oms/monthly.jsx
+++ src/pages/oms/monthly.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { getMonthlyOrder, generateGoogleSheet } from "@/services/order";
 
 import { getCurrentMonth, getNextMonth } from "@/helpers/utils";
@@ -6,11 +6,14 @@ import Loader from "@/components/Loader";
 
 import { toast } from "react-toastify";
 import classNames from "classnames";
+import Tabs from "@/components/Tabs";
+import MonthlyOrderTableHeader from "@/components/MonthlyOrderTableHeader";
 
 const MonthlyOrder = () => {
   const [ingredients, setIngredients] = useState();
   const [loading, setLoading] = useState(false);
   const [generateOrderInProgress, setGenerateOrder] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("Buy");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -62,113 +65,102 @@ const MonthlyOrder = () => {
   const currentMonth = getCurrentMonth();
   const nextMonth = getNextMonth();
 
+  const tabs = useMemo(() => {
+    return ["All", "Buy", "Sell", "Redundant"];
+  }, []);
+
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4">
         Current Month - {currentMonth}, Monthly Order Generation for -{" "}
         {nextMonth}
       </h2>
+      <div classNma>
+        <Tabs
+          tabs={tabs}
+          selected={selectedTab}
+          setSelectedTab={(tab) => setSelectedTab(tab)}
+        />
+      </div>
       <div className="my-4">
         {ingredients ? (
-          <table className="min-w-full divide-y divide-gray-200 max-w-[100%]">
-            <thead className="bg-gray-50 max-w-[100%] sticky top-[100px]">
-              <tr className="bg-gray-200">
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  No.
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Ingredient
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Requirement (Next Month)
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Requirement (Tmrw to Month end)
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Current Stock (By end of today)
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Closing Stock (Tmrw to Month end)
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Bulk order
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Adjustment
-                </th>
-                <th className="px-3 py-2 font-bold uppercase tracking-wider">
-                  Purchase Unit
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200 max-w-[100%]">
-              {ingredients &&
-                ingredients.map((ingredient, index) => {
-                  const {
-                    _id,
-                    name,
-                    bulkOrder,
-                    monthlyMealPlan,
-                    remainingMealPlan,
-                    currentStock,
-                    adjustment,
-                    purchaseUnit,
-                    closingStock,
-                  } = ingredient;
-                  return (
-                    <tr className="border-b max-w-[100%]" key={_id}>
-                      <td className="px-3 py-2 whitespace-nowrap capitalize">
-                        {index + 1}
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap font-bold">
-                        {name}
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap text-center">
-                        {monthlyMealPlan}
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap text-center">
-                        {remainingMealPlan}
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap text-center">
-                        {currentStock}
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap text-center">
-                        {closingStock}
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap text-center">
-                        {bulkOrder}
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap text-center">
-                        <input
-                          type="text"
-                          onChange={(e) => {
-                            const updatedIngredients = ingredients.map(
-                              (ing) => {
-                                // console.log({ ingredient, ing });
-                                if (ing._id === ingredient._id) {
-                                  return {
-                                    ...ingredient,
-                                    adjustment: e.target.value,
-                                  };
-                                }
-                                return ing;
-                              }
-                            );
-                            setIngredients(updatedIngredients);
-                          }}
-                          value={adjustment}
-                          className="pl-10 pr-4 py-2 border rounded-md"
-                        />
-                      </td>
-                      <td className="px-3 py-2 whitespace-nowrap text-center">
-                        {purchaseUnit}
-                      </td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </table>
+          Object.keys(ingredients[selectedTab.toLowerCase()]).map((vendor) => {
+            return (
+              <div className="my-14" key={vendor}>
+                <h2 className="font-bold text-lg">{vendor}</h2>
+                <table className="min-w-full divide-y divide-gray-200 max-w-[100%]">
+                  <MonthlyOrderTableHeader />
+                  <tbody className="bg-white divide-y divide-gray-200 max-w-[100%]">
+                    {ingredients[selectedTab.toLowerCase()][vendor].map(
+                      (ingredient, index) => {
+                        const {
+                          _id,
+                          name,
+                          bulkOrder,
+                          monthlyMealPlan,
+                          remainingMealPlan,
+                          currentStock,
+                          adjustment,
+                          purchaseUnit,
+                          closingStock,
+                        } = ingredient;
+                        return (
+                          <tr className="border-b max-w-[100%]" key={_id}>
+                            <td className="px-3 py-2 whitespace-nowrap capitalize">
+                              {index + 1}
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap font-bold max-w-[300px] overflow-hidden text-ellipsis">
+                              {name}
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap text-center">
+                              {monthlyMealPlan}
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap text-center">
+                              {remainingMealPlan}
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap text-center">
+                              {currentStock}
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap text-center">
+                              {closingStock}
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap text-center">
+                              {bulkOrder}
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap text-center">
+                              <input
+                                type="text"
+                                onChange={(e) => {
+                                  const updatedIngredients = ingredients.map(
+                                    (ing) => {
+                                      // console.log({ ingredient, ing });
+                                      if (ing._id === ingredient._id) {
+                                        return {
+                                          ...ingredient,
+                                          adjustment: e.target.value,
+                                        };
+                                      }
+                                      return ing;
+                                    }
+                                  );
+                                  setIngredients(updatedIngredients);
+                                }}
+                                value={adjustment}
+                                className="pl-10 pr-4 py-2 border rounded-md"
+                              />
+                            </td>
+                            <td className="px-3 py-2 whitespace-nowrap text-center">
+                              {purchaseUnit}
+                            </td>
+                          </tr>
+                        );
+                      }
+                    )}
+                  </tbody>
+                </table>
+              </div>
+            );
+          })
         ) : (
           <div className="w-full text-center flex justify-center items-center">
             <Loader />
@@ -176,7 +168,7 @@ const MonthlyOrder = () => {
         )}
       </div>
 
-      <div className="mt-10">
+      {/* <div className="mt-10">
         <button
           type="submit"
           className={classNames(
@@ -189,7 +181,7 @@ const MonthlyOrder = () => {
         >
           {generateOrderInProgress ? <Loader /> : "Generate Purchase order"}
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
