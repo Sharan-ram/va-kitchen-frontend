a9833bb8bb6d22dc0fed0d4f9ef9eea57189afd7
Sharan R <sharanram1993@gmail.com>
2024-09-19 17:19:21 +0530

    Test

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,10 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../global.css";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "@/components/Layout";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { ToastContainer } from "react-toastify";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-import { Analytics } from "@vercel/analytics/react";
 
 const unprotectedRoutes = [
   "/user/login",
@@ -31,6 +30,7 @@ export default function App({ Component, pageProps }) {
       {/* <Layout /> */}
       <div className="max-w-[1500px] 2xl:mx-auto pt-[120px] pb-10 xl:mx-[40px]">
         <Component {...pageProps} />
+        <SpeedInsights />
         {/* <SpeedInsights /> */}
         {/* <Analytics />
         <ToastContainer /> */}
