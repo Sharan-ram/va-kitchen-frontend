083103644e5bd66a84d6d6015bfffb0b5226f1c2
Sharan R <sharanram1993@gmail.com>
2024-10-07 17:36:06 +0530

    Show summerQuantity,monsoonQuantity and winterQuantity values for bulk order

--- src/pages/ingredients/bulkOrder/manual-update.jsx
+++ src/pages/ingredients/bulkOrder/manual-update.jsx
@@ -55,38 +55,60 @@ const ManualBulkOrderUpdate = () => {
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4">Manual Bulk Order Update</h2>
+      <div className="my-10 flex items-center justify-between">
+        <div className="w-[40%]"></div>
+        <div className="w-[60%] flex items-center justify-between">
+          {["Summer Quantity", "Monsoon Quantity", "Winter Quantity"].map(
+            (text) => {
+              return (
+                <div className="w-[30%] text-lg font-bold" key={text}>
+                  {text}
+                </div>
+              );
+            }
+          )}
+        </div>
+      </div>
       {ingredients && !bulkOrderLoading ? (
         ingredients.map((ingredient) => {
           return (
-            <div key={ingredient._id} className="flex items-center w-1/2 my-4">
-              <div className="w-1/2">
+            <div key={ingredient._id} className="flex items-center w-full my-4">
+              <div className="w-[40%]">
                 <p className="font-semibold">{ingredient.name}</p>
               </div>
-              <div className="w-1/2 flex items-center">
-                <div>
-                  <input
-                    type="text"
-                    onChange={(e) => {
-                      const updatedIngredients = ingredients.map((ing) => {
-                        // console.log({ ingredient, ing });
-                        if (ing._id === ingredient._id) {
-                          return {
-                            ...ingredient,
-                            bulkOrder: e.target.value,
-                          };
-                        }
-                        return ing;
-                      });
-                      setIngredients(updatedIngredients);
-                    }}
-                    value={ingredient.bulkOrder}
-                    className="pl-10 pr-4 py-2 border rounded-md"
-                  />
-                </div>
+              <div className="w-[60%] flex items-center justify-between">
+                {["summerQuantity", "monsoonQuantity", "winterQuantity"].map(
+                  (quantity) => {
+                    return (
+                      <div key={quantity} className="w-[30%]">
+                        <input
+                          type="text"
+                          onChange={(e) => {
+                            const updatedIngredients = ingredients.map(
+                              (ing) => {
+                                // console.log({ ingredient, ing });
+                                if (ing._id === ingredient._id) {
+                                  return {
+                                    ...ingredient,
+                                    bulkOrder: e.target.value,
+                                  };
+                                }
+                                return ing;
+                              }
+                            );
+                            setIngredients(updatedIngredients);
+                          }}
+                          value={ingredient.bulkOrder[quantity] || ""}
+                          className="pl-10 pr-4 py-2 border rounded-md"
+                        />
+                      </div>
+                    );
+                  }
+                )}
 
-                <div className="ml-2">
+                {/* <div className="ml-2">
                   <p>{ingredient.purchaseUnit}</p>
-                </div>
+                </div> */}
               </div>
             </div>
           );
