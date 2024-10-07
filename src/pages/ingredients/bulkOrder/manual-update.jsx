97b169cd60b85bd831a1decaafb1ff9717929f52
Sharan Ram <sharanram1993@gmail.com>
2024-10-07 21:00:31 +0530

    Update bulk order with summer, winter and monsoon quantities (#24)
    * Show summerQuantity,monsoonQuantity and winterQuantity values for bulk order
    
    * Implement update bulk order feature
    
    * Fixup

--- src/pages/ingredients/bulkOrder/manual-update.jsx
+++ src/pages/ingredients/bulkOrder/manual-update.jsx
@@ -15,7 +15,12 @@ const ManualBulkOrderUpdate = () => {
       try {
         setBulkOrderLoading(true);
         const res = await getIngredientBulkOrder();
-        setIngredients(res);
+        // console.log({ res });
+        let ingredientObj = {};
+        res.forEach((ing) => {
+          ingredientObj[ing._id] = ing;
+        });
+        setIngredients(ingredientObj);
         setBulkOrderLoading(false);
       } catch (e) {
         console.error(e);
@@ -27,25 +32,38 @@ const ManualBulkOrderUpdate = () => {
     fetchIngredients();
   }, []);
 
+  // console.log({ ingredients });
+
   //   console.log({ ingredients });
   const handleSubmit = async () => {
     try {
       setUpdateBulkOrderLoading(true);
-      const newIngredients = ingredients.map((ing) => {
+      const newIngredients = Object.keys(ingredients).map((ingId) => {
+        // console.log({
+        //   id: ingId,
+        //   summerQuantity: ingredients[ingId].summerQuantity,
+        // });
         return {
-          ...ing,
-          bulkOrder: Number(ing.bulkOrder),
+          _id: ingId,
+          bulkOrder: {
+            summerQuantity:
+              parseFloat(ingredients[ingId].bulkOrder.summerQuantity) || null,
+            monsoonQuantity:
+              parseFloat(ingredients[ingId].bulkOrder.monsoonQuantity) || null,
+            winterQuantity:
+              parseFloat(ingredients[ingId].bulkOrder.winterQuantity) || null,
+          },
         };
       });
       await updateBulkOrder({
         ingredients: newIngredients,
       });
       setUpdateBulkOrderLoading(false);
-      toast.success("Prices updated successfully!");
+      toast.success("Bulk orders updated successfully!");
     } catch (e) {
       console.error(e);
       setUpdateBulkOrderLoading(false);
-      toast.error("Error updating prices!");
+      toast.error("Error updating bulk orders!");
     }
   };
 
@@ -54,39 +72,78 @@ const ManualBulkOrderUpdate = () => {
 
   return (
     <div>
-      <h2 className="text-xl font-semibold mb-4">Manual Bulk Order Update</h2>
+      <div className="w-full flex items-center justify-between">
+        <div className="w-[40%]">
+          <h2 className="text-xl font-semibold mb-4">
+            Manual Bulk Order Update
+          </h2>
+        </div>
+        <div className="w-[60%]">
+          <button
+            type="submit"
+            className={classNames(
+              "px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600",
+              disableUpdateButton && "cursor-not-allowed opacity-50"
+            )}
+            onClick={handleSubmit}
+            disabled={disableUpdateButton}
+          >
+            {updateBulkOrderLoading ? <Loader /> : "Update Bulk orders"}
+          </button>
+        </div>
+      </div>
+      <div className="my-10 flex items-center justify-between">
+        <div className="w-[40%]"></div>
+        <div className="w-[60%] flex items-center justify-between">
+          {["Summer Quantity", "Monsoon Quantity", "Winter Quantity"].map(
+            (text) => {
+              return (
+                <div className="w-[30%] text-lg font-bold" key={text}>
+                  {text}
+                </div>
+              );
+            }
+          )}
+        </div>
+      </div>
       {ingredients && !bulkOrderLoading ? (
-        ingredients.map((ingredient) => {
+        Object.keys(ingredients).map((ingredientId) => {
+          const ingredient = ingredients[ingredientId];
           return (
-            <div key={ingredient._id} className="flex items-center w-1/2 my-4">
-              <div className="w-1/2">
+            <div key={ingredient._id} className="flex items-center w-full my-4">
+              <div className="w-[40%]">
                 <p className="font-semibold">{ingredient.name}</p>
               </div>
-              <div className="w-1/2 flex items-center">
-                <div>
-                  <input
-                    type="text"
-                    onChange={(e) => {
-                      const updatedIngredients = ingredients.map((ing) => {
-                        // console.log({ ingredient, ing });
-                        if (ing._id === ingredient._id) {
-                          return {
-                            ...ingredient,
-                            bulkOrder: e.target.value,
-                          };
-                        }
-                        return ing;
-                      });
-                      setIngredients(updatedIngredients);
-                    }}
-                    value={ingredient.bulkOrder}
-                    className="pl-10 pr-4 py-2 border rounded-md"
-                  />
-                </div>
+              <div className="w-[60%] flex items-center justify-between">
+                {["summerQuantity", "monsoonQuantity", "winterQuantity"].map(
+                  (quantity) => {
+                    return (
+                      <div key={quantity} className="w-[30%]">
+                        <input
+                          type="text"
+                          onChange={(e) => {
+                            setIngredients({
+                              ...ingredients,
+                              [ingredientId]: {
+                                ...ingredients[ingredientId],
+                                bulkOrder: {
+                                  ...ingredients[ingredientId].bulkOrder,
+                                  [quantity]: e.target.value,
+                                },
+                              },
+                            });
+                          }}
+                          value={ingredient.bulkOrder[quantity] || ""}
+                          className="pl-10 pr-4 py-2 border rounded-md"
+                        />
+                      </div>
+                    );
+                  }
+                )}
 
-                <div className="ml-2">
+                {/* <div className="ml-2">
                   <p>{ingredient.purchaseUnit}</p>
-                </div>
+                </div> */}
               </div>
             </div>
           );
